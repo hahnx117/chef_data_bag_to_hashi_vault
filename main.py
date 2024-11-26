d57abc6313a2b5e19c1c75060f45612337374f31
David Hahn <hahnx117@umn.edu>
2024-11-26 09:45:19 -0600

    generalize for any data bag

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ SECRET_ID = os.getenv("SECRET_ID")
 MOUNT_POINT = os.getenv("MOUNT_POINT")
 VAULT_PATH = os.getenv("VAULT_PATH")
 VAULT_URL = os.getenv("VAULT_URL")
-#METHOD_TYPE = os.getenv("METHOD_TYPE")
 USER_TOKEN = os.getenv("USER_TOKEN")
+DATA_BAG = "local_admins"
+#METHOD_TYPE="approle"
 
-print(SECRET_ID)
+admin_list = ["hahnx117", "kalle001", "olsont", "dolsen", "cbs"]
 
 ## SIGN IN WITH APPROLE
 client = hvac.Client(url=VAULT_URL)
@@ -38,35 +39,39 @@ if client.is_authenticated():
 else:
     raise Exception("Authentication failed.")
 
-## GET THE CHEF USER LIST
-#user_list = subprocess.check_output(["knife", "data", "bag", "show", "local_users"], encoding="utf-8").split("\n")
+## GET THE CHEF DATA BAG ITEMS
+data_bag_items = subprocess.check_output(["knife", "data", "bag", "show", DATA_BAG], encoding="utf-8").split("\n")
 
-#for i in user_list[0:10]:
-for i in ["class_tasks"]:
+for i in data_bag_items:
+#for i in ["local_hahnx117"]:
     try:
         # Execute the command and capture the output
-        result = subprocess.run(["knife", "data", "bag", "show", "local_users", i, "-F", "json"], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        #import pdb; pdb.set_trace()
+        result = subprocess.run(["knife", "data", "bag", "show", DATA_BAG, i, "-F", "json"], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
         # Parse the JSON output
         json_output = json.loads(result.stdout)
 
-        if "disable" not in json_output.keys():
+        if "disable" not in json_output.keys() and json_output['id'] not in admin_list:
             pprint(json_output)
+            endpont = json_output.pop('id')
+            #import pdb; pdb.set_trace()
             try:
                 response = client.secrets.kv.v2.create_or_update_secret(
-                    path=f"{VAULT_PATH}/{json_output['id']}",
+                    path=f"{VAULT_PATH}/{DATA_BAG}/{endpont}",
                     secret=json_output,
                     mount_point=MOUNT_POINT
                 )
                 print(response)
-                print(f"Secret written successfully to {MOUNT_POINT}/{VAULT_PATH}/{json_output['id']}")
+                print(f"Secret written successfully to {MOUNT_POINT}/{VAULT_PATH}/{DATA_BAG}/{endpont}\n")
             except hvac.exceptions.InvalidPath as e:
                 print(f"Invalid path error: {e}")
             except Exception as e:
                 print(f"An error occurred: {e}")
         else:
-            print(f"{json_output['id']} is disabled.")
+            if json_output['id'] in admin_list:
+                print(f"{json_output['id']} is already in there.")
+            elif "disable" in json_output.keys():
+                print(f"{json_output['id']} is disabled.")
 
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while executing the command: {e.stderr}")
